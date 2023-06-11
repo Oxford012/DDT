6380237b4c106c6808129e788b0f285fcf82ecf1
Florian Lichius <flichius@smail.uni-koeln.de>
2023-06-11 21:51:42 +0200

    idle time calculation

--- Florian/weather.py
+++ Florian/weather.py
@@ -6,7 +6,7 @@ def getCleanWeather(pathToWeatherCSV):
     weather = pd.read_csv(pathToWeatherCSV)
     weather['timestamp'] = pd.to_datetime(weather['timestamp'])
     weather = weather[(weather['timestamp'].dt.year >= 2017) & (weather['timestamp'].dt.year <= 2018)]
-    weather.reset_index(drop=True, inplace=True)
     weather = weather.dropna(axis=0, how='any')
+    weather.reset_index(drop=True, inplace=True)
     return weather
 
